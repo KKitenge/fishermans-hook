05c2ee32aa09728376e5dbb367ba9e00488f4484
unknown <eogbeide2@gmail.com>
2023-06-07 10:27:33 -0500

    added clientside queries and mutations and added apollo client to app.js

--- server/schemas/openai.js
+++ server/schemas/openai.js
@@ -1,17 +1,18 @@
-const { Configuration,  OpenAIApi } = require('openai');
-require('dotenv').config();
+import { Configuration, OpenAIApi } from "openai";
+import dotenv from "dotenv";
 
 
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    organization: "org-YLNfaOCVsvPpQKVV3HN6Q391",
+    apiKey:"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" ,
   });
 
   const openai = new OpenAIApi(configuration);
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: "Hello world"}],
-  });
+    messages: [{role: "user", content: "Hello world"},]
+  })
 
 console.log(completion.data.choices[0].message);
 

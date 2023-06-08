e0c82f54fedb4032b7a84bf90f2562414295dacd
Ss0110 <saraysanchez01@yahoo.com>
2023-06-08 16:22:34 -0700

    made corrections to login/signup

--- client/fisherman-hook/src/components/auth/Login.js
+++ client/fisherman-hook/src/components/auth/Login.js
@@ -1,77 +1,77 @@
 // imported necessary modules
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from 'react-redux';
-import { LOGIN_USER } from '../../utils/mutations';
-import Auth from '../../actions/auth';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
 
 // Login page component
 const LoginPage = () => {
-    const [formState, setFormState] = useState({
-        username: '',
-        email: '',
-        password: '',
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [login] = useMutation(LOGIN_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
     });
-    const [login, { error, data }] = useMutation(LOGIN_USER);
+  };
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
 
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
 
-    // submit form
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        console.log(formState);
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+    }
 
-        try {
-            const { data } = await login({
-                variables: {...formState}
-            });
+    // clear form values
+    setFormState({
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
 
-            Auth.login(data.login.token);
-        } catch (err) {
-            console.error(err);
-        }
-
-        // clear form values
-        setFormState({
-            username: '',
-            email: '',
-            password: '',
-        });
-    };
-
-    // return jsx, form for email and password
-    return (
-        <main className="login-page">
-            <h1>Login</h1>
-            <form onSubmit={handleFormSubmit}>
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    value={formState.email}
-                    onChange={handleInputChange}
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    value={formState.password}
-                    onChange={handleInputChange}
-                />
-                <button type="submit">Login</button>
-            </form>
-            <p>
-                Don't have an account? <Link to="/register">Register</Link>
-            </p>
-        </main>
-    );
+  // return jsx, form for email and password
+  return (
+    <main className="login-page">
+      <h1>Login</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formState.email}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formState.password}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </main>
+  );
 };
 
 export default LoginPage;

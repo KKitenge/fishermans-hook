ec982a21bffdb59ec6be33b88d760941d8d3f485
Ss0110 <saraysanchez01@yahoo.com>
2023-06-08 17:16:18 -0700

    worked on forecast file

--- client/fisherman-hook/src/utils/queries.js
+++ client/fisherman-hook/src/utils/queries.js
@@ -1,51 +1,51 @@
-import {gql} from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const QUERY_ME= gql`
-    query me {
-        me {
-            _id
-            username
-            firstName
-            email
-            friends {
-                _id
-                username
-                firstName
-                email
-            }
-            trips {
-                _id
-                tripText
-                username
-                createdAt
-            }
-            posts {
-                _id
-                postTitle
-                postText
-                postAuthor
-                createdAt
-                comments {
-                    _id
-                    commentText
-                    commentAuthor
-                    createdAt
-                }
-            }
-            comments {
-                _id
-                commentText
-                commentAuthor
-                createdAt
-            }
-            messages {
-                _id
-                messageText
-                messageAuthor
-                createdAt
-            }
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      firstName
+      email
+      friends {
+        _id
+        username
+        firstName
+        email
+      }
+      trips {
+        _id
+        tripText
+        username
+        createdAt
+      }
+      posts {
+        _id
+        postTitle
+        postText
+        postAuthor
+        createdAt
+        comments {
+          _id
+          commentText
+          commentAuthor
+          createdAt
         }
+      }
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+      messages {
+        _id
+        messageText
+        messageAuthor
+        createdAt
+      }
     }
+  }
 `;
 
 export const QUERY_USERS = gql`
@@ -86,125 +86,122 @@ export const QUERY_USERS = gql`
 `;
 
 export const QUERY_USER = gql`
-    query user($username: String!) {
-        user(username:$username) {
-            _id
-            username
-            firstName
-            email
-            friends {
-                _id
-                username
-                firstName
-                email
-            }
-            trips {
-                _id
-                tripText
-                username
-                createdAt
-            }
-            posts {
-                _id
-                postTitle
-                postText
-                postAuthor
-                createdAt
-                comments {
-                    _id
-                    commentText
-                    commentAuthor
-                    createdAt
-                }
-            }
-            comments {
-                _id
-                commentText
-                commentAuthor
-                createdAt
-            }
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      firstName
+      email
+      friends {
+        _id
+        username
+        firstName
+        email
+      }
+      trips {
+        _id
+        tripText
+        username
+        createdAt
+      }
+      posts {
+        _id
+        postTitle
+        postText
+        postAuthor
+        createdAt
+        comments {
+          _id
+          commentText
+          commentAuthor
+          createdAt
         }
+      }
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
     }
+  }
 `;
 
 export const QUERY_POSTS = gql`
-    query posts {
-        posts {
-            _id
-            postTitle
-            postText
-            postAuthor
-            createdAt
-            comments {
-                _id
-                commentText
-                commentAuthor
-                createdAt
-            }
-        }
+  query posts {
+    posts {
+      _id
+      postTitle
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
     }
+  }
 `;
 
 export const QUERY_SINGLE_POST = gql`
-    query singlePost($postId: ID!) {
-        singlePost(postId:$postId) {
-            _id
-            postTitle
-            postText
-            postAuthor
-            createdAt
-            comments {
-                _id
-                commentText
-                commentAuthor
-                createdAt
-            }
-        }
+  query singlePost($postId: ID!) {
+    singlePost(postId: $postId) {
+      _id
+      postTitle
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
     }
+  }
 `;
 
-export const QUERY_FORCAST = gql`
-    query forcast($city: String!) {
-        forcast(city:$city) {
-            Headline {
-                text
-                category
-            }
-            DailyForecasts {
-                Date
-                Temperature {
-                    minimum {
-                        Value
-                        Unit
-                    }
-                    maximum {
-                        Value
-                        Unit
-                    }
-                }
-                Day {
-                    Icon
-                    IconPhrase
-                }
-                Night {
-                    Icon
-                    IconPhrase
-                }
-            }
+export const QUERY_FORECAST = gql`
+  query forecast($city: String!) {
+    forecast(city: $city) {
+      Headline {
+        text
+        category
+      }
+      DailyForecasts {
+        Date
+        Temperature {
+          minimum {
+            Value
+            Unit
+          }
+          maximum {
+            Value
+            Unit
+          }
         }
+        Day {
+          Icon
+          IconPhrase
+        }
+        Night {
+          Icon
+          IconPhrase
+        }
+      }
     }
+  }
 `;
 
 export const QUERY_TRIPS = gql`
-    query trips {
-        trips {
-            _id
-            tripText
-            username
-            createdAt
-        }
+  query trips {
+    trips {
+      _id
+      tripText
+      username
+      createdAt
     }
+  }
 `;
-
-
-
